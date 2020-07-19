96a8be090ee7e1b5fa371dc545a5d5ee0d9efd2e
pallenJ <pallennellap17@gmail.com>
2020-07-19 14:46:29 +0900

    수정까지 완료

--- pallenBird/routes/articles.js
+++ pallenBird/routes/articles.js
@@ -11,6 +11,7 @@ router.get('/', async(req,res,next)=>{
     let page = req.param('pg');
     console.log(page);
     if(page==undefined||page<=0) page =1;
+    res.cookie('pg',page)
     const article_list = await Article.findAll(
         {
             include: {
@@ -20,7 +21,7 @@ router.get('/', async(req,res,next)=>{
             order:[['id','DESC']],
             limit:9,
             offset:(page-1)*9,
-        });
+        }); 
         res.render('articles/article_list',{article_list});
 });
 
@@ -39,7 +40,8 @@ router.post('/add',async(req,res,next)=>{
         })
         jsonData = {board,title,content,userId:loginInfo.id,img:req.files};
     }
-    res.send(jsonData);
+    //res.send(jsonData);
+    res.redirect('/')
 });
 
 //Read
@@ -53,11 +55,56 @@ router.get('/:id',async(req,res,next)=>{
             id:articleId
         }
     }); 
-    res.render('articles/article_detail',{article_data});
+    res.render('articles/article_detail',{article_data,loginInfo:req.session.loginInfo});
 });
 
 //Update
+router.get('/edit/:id',async(req,res,next)=>{
+    const articleId = req.params.id;
+    const article_data = await Article.findOne({
+        include:{
+            model:User 
+        },
+        where:{
+            id:articleId
+        }
+    }); 
+    res.render('articles/article_edit',{article_data,loginInfo:req.session.loginInfo});
+});
+
+
 router.patch('/:id',async(req,res,next)=>{
+    console.log('patch');
+    let jsonData = {};
+    const articleId = req.params.id;
+    const loginInfo = req.session.loginInfo;
+    const article_data = await Article.findOne({
+        where:{
+            id:articleId
+        }
+    }); 
+    if(!article_data) {//해당 글이 없을시
+        jsonData={message:'article is not exist'};
+    }else if(loginInfo&&loginInfo.id == article_data.userId){
+        let {title, board, content} = req.body;
+        //수정내용이 없을시 기존내용입력
+        // title = title||article_data.title;
+        // board = board||article_data.board;
+        // content = content||article_data.content;
+        
+        let edit = await Article.update({
+            title,board,content
+        },{ 
+            where:{ id:articleId } 
+        });
+        console.log('edit:'+edit);
+        jsonData={message:'update success',title:title,board:board,content:content,userId:article_data.userId}
+    }
+    res.json(jsonData)
+});
+
+router.post('/edit/:id',async(req,res,next)=>{
+    
     let jsonData = {};
     const articleId = req.params.id;
     const loginInfo = req.session.loginInfo;
@@ -86,6 +133,8 @@ router.patch('/:id',async(req,res,next)=>{
     res.json(jsonData)
 });
 
+
+//Delete
 router.delete('/:id',async(req,res,next)=>{
     let jsonData = {};
     const articleId = req.params.id;
