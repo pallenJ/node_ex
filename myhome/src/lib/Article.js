f5e693fe017f1111798dbf734149aee09605e162
JoonMo <pallennellap17@gmail.com>
2021-06-11 18:31:59 +0900

    예제중

--- myhome/src/lib/Article.js
+++ myhome/src/lib/Article.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class Article extends Component{
     constructor(props){
@@ -8,19 +9,39 @@ class Article extends Component{
     }
 
 
-    getPiece = (_key,val)=>this.props.isHeader?
+    getArticleData = (_key,val)=>this.props.isHeader?
     <th key={_key} className =  'text text-center'> {val}</th>:<td key={_key}>{val}</td>;
 
     render(){
-
+        const {canEdit} = this.props;
         return(
             <tr>
                 {
 
                     Object.keys(this.state).map(_key=>{
-                       return this.getPiece(_key,this.state[_key]);
+                       return this.getArticleData(_key,this.state[_key]);
                     })
-
+                    
+                }
+                {
+                    
+                    <td>
+                        {canEdit?(
+                            <div className="align-items-center" role="group" aria-label="Basic example" >
+                                
+                                <Button variant='outline-primary' className = 'align-left'>
+                                    Edit
+                                  </Button>
+                                  &nbsp;
+                                  &nbsp;
+                                  &nbsp;
+                                <Button variant='outline-danger' align={'end'}>
+                                    Delete
+                                </Button>
+                            </div>
+                        ):null}
+                    </td>
+                    
                 }
             </tr>
         )
--- myhome/src/routes/Example.js
+++ myhome/src/routes/Example.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
-import {Table}from 'react-bootstrap'
+import {Button, Col, Row, Table}from 'react-bootstrap'
 import Article from '../lib/Article';
 import dateFormat from 'dateformat'
 const Example=({match})=>{
+    
     return(
     <div>
        <Route exact path={`${match.path}`} component={aaa} />
@@ -18,37 +19,86 @@ const aaa =  ()=>{
         fdsf
     </div>)
 }
-
+const exAuth = {canEdit:true};
 const EX1= ()=>{
+    let articleData = [];
     let _cnt = 0;
-    let last= 0;
-    let articleList = [];
-    while(_cnt<5 ){
-        articleList.push(<Article key = {_cnt} article={{bno:`${_cnt}`,writer:`writer ${_cnt}`,content:`content${_cnt}`,
-        added:`${dateFormat(Date.now() -(6-_cnt)*24 *60*60*1000 -Math.floor(Math.random() * 24 *60*60*1000),'yyyy-mm-dd HH:MM:ss' )}`,
-        edited:`${dateFormat(Date.now() -(5-_cnt)*24 *60*60*1000 -Math.floor(Math.random() * 24 *60*60*1000),'yyyy-mm-dd HH:MM:ss' )}`}} isHeader = 'false'/>)
-        console.log(Date.now());
-        console.log(Math.floor(Math.random()*(5-_cnt) * 24 *60*60 *1000000 ));
-        _cnt++;
+    const [articles, setarticles] = useState(articleData);
+    const [last, setlast] = useState(5);
+    const getArticle = (_key,data,authority = {})=>{
+        
+        return(
+            <Article key = {_key} article={
+                {
+                    bno:`${data.bno}`,writer:`${data.writer}`,
+                    content:`${data.content}`,
+                    added:`${dateFormat(data.added,'yyyy-mm-dd HH:MM:ss' )}`,
+                    edited:`${dateFormat(data.edited,'yyyy-mm-dd HH:MM:ss' )}`}} isHeader = 'false' canEdit ={authority.canEdit}/>
+                    );
     }
-    last = _cnt;
+    while(_cnt<5 ){
+        articleData.push(getArticle(_cnt,{bno:`${_cnt}`,writer:`writer ${_cnt}`,content:`content${_cnt}`,
+        added:dateFormat(Date.now() -(6-_cnt)*24 *60*60*1000 -Math.floor(Math.random() * 24 *60*60*1000),'yyyy-mm-dd HH:MM:ss' ),
+        edited:dateFormat(Date.now() -(5-_cnt)*24 *60*60*1000 -Math.floor(Math.random() * 24 *60*60*1000),'yyyy-mm-dd HH:MM:ss' )},
+        {canEdit: true}
+        )
+        );
+        _cnt++;
+        
+        //setlast(5);
+    }
+    
+    
+    const addArticleEX = (authority)=>{
+        const pairData = {bno:last,writer:`writer${last}`,content:`content${last}`,added:Date.now(),edited:Date.now()};
+        const newArticle = getArticle(last,pairData,authority);
+        //articleData.push(newArticle);
+
+        console.log(articles);
+        console.log(last);
+        setarticles(
+            articles.concat(
+                [newArticle]
+            )
+        );
+        setlast(last+1);
+        
+    }
+
+
 
     return(
-        <div>
+        <div className ='container-fluid'>
             <Table className = 'table table-dark table-striped'>
                 <thead>
 
                 <Article article={{bno:'bno',writer:'writer',content:'content',added:'added',edited:'edited'}} isHeader = 'true'/>
                 </thead>
                 <tbody>
-                    {
-                        articleList.map(elt => elt)
-                    }
-                </tbody>
 
+                    {
+                        articles.map(elt => elt)
+                    }
+                    
+                </tbody>
+                <tfoot>
+                    <tr color = 'white' className ='table-white'>
+                        <td colSpan = '100%' align ='right'>
+
+                            <Button className ='add' variant ='success' size ='large' onClick ={()=>{addArticleEX({canEdit:true})}}> 
+                                ADD
+                            </Button>
+
+                            
+                        </td>
+                    </tr>
+                </tfoot>
             </Table>
+           
         </div>
     );
+
+   
 }
 
 const EX2= ()=>{
