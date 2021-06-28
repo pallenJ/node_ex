512f201c76319445db80cdee86bacbbfa5044890
JoonMo <pallennellap17@gmail.com>
2021-06-28 21:52:54 +0900

    과제 완료

--- my-homework/src/App.tsx
+++ my-homework/src/App.tsx
@@ -4,24 +4,23 @@ import service from './service/service'
 
 function App() {
   const [page, setpage] = useState<number>(0);
-  const [list, setlist] = useState<never[]>()
-  const [search, setsearch] = useState("")
+  const [list, setlist] = useState<never[]>();
+  const [search, setsearch] = useState("");
+  const [post, setpost] = useState('a');
+  const [selected, setselected] = useState<any>(null);
 
+  const moreShow = (clear = false, _search = search,_post  = post) => {
+    const _page = clear ? 0 : page;
 
-
-   
-  const moreShow = (clear = false,_search=search)=>{
-    const _page = clear?0: page;
-
-    service.getList({page:_page,search:_search}).then(e=>{
-      if((e as never[]).length == 0&&!clear)return;
-      else if(clear){
+    service.getList({ page: _page, search: _search},_post).then(e => {
+      if ((e as never[]).length == 0 && !clear) return;
+      else if (clear) {
         setlist([]);
-      }else{
+      } else {
 
         const temp = list ?? [];
-        setlist(clear? e : temp.concat(e));
-        setpage(_page+1);
+        setlist(clear ? e : temp.concat(e));
+        setpage(_page + 1);
       }
     })
   }
@@ -40,38 +39,68 @@ function App() {
       moreShow();
     }
   };
+  const searchFnc = (e: any) => {
+    setsearch(e.target.value)
+    setpage(0);
+    moreShow(true, e.target.value);
+    console.log(e.target.value)
+  };
+
+  const showDetail = (e:any) =>
+(
+    <div style = {{textAlign:'center'}}>
+      <div style = {{border:'1px solid black',width:'64%',left:'18%',right:'18%', position:'relative'}}>
+        <h2>{e.title}</h2>
+        <div style={{padding:'20px'}}>
+          {e.content}
+        </div>
+      </div>
+      <button className = 'btn-selected' onClick = {()=>{setselected(null) }}>뒤로가기</button>
+    </div>)
 
   //infiniteScroll();
   useEffect(() => {
     infiniteScroll();
-    window.addEventListener('scroll',infiniteScroll);
+    window.addEventListener('scroll', infiniteScroll);
     return () => {
-      window.removeEventListener('scroll',infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
     };
-  }, [{page}]);
-  return (
-    <div className="App">
-      <h1>HOME WORK</h1>
-      <h4>PAGE:{page}</h4>
-      <input type="search" onChange = {(e)=>{
-        setsearch(e.target.value)
-        setpage(0);
-        moreShow(true,e.target.value);
-        console.log(e.target.value)
-      }}
+  }, [{ page }]);
+  const Main = <div style = {{border:'1px solid black', padding:'50px'}}>
+            <div className="wrap" >
+   <div className="search">
+      <input type="text" className="searchTerm" placeholder = 'Search...' onChange={searchFnc}/>
 
-      onBlur = {()=>{console.log('sdf')}}
-      />
-      <hr/>
+   </div>
+</div>
+   <button className = {`btn${post ==='a'?'-selected':''}`} value = 'A-post' onClick = {()=>{setpost('a');setpage(0);moreShow(true,search,'a');}}>A-post</button>
+   <button className = {`btn${post ==='b'?'-selected':''}`} value = 'A-post' onClick = {()=>{setpost('b');setpage(0);moreShow(true,search,'b');}}>B-post</button>
+      <hr />
       <div>
-        {
-          list?.map(
-            e=><pre>{JSON.stringify(e)}</pre>
-          )
-          
-        }
+        <ul className = ''>
+          {
+
+            list?.map(
+              (e:any) => <li  style = {{userSelect:'text',margin:'20px',listStyle:'none'}} className ='bg-gray-100'>
+                <article onClick={() => { setselected(showDetail(e));}}
+              >
+               <h3><span style = {{color:'blueviolet'}}> {e.id}</span>&nbsp;<span style={{}}>{e.title}</span></h3>
+               <span className = 'pre'>{e.content}</span>
+                </article></li>
+            )
+
+          }
+        </ul>
       </div>
-      <hr/>
+  </div>
+
+  return (
+    <div className="App" style = {{width:'70%', left:'15%',right:'15%',position:'relative'}}>
+      <h1 style = {{textAlign:'center'}}>HOMEWORK</h1>
+        {console.log(selected === null)}
+        {console.log(selected)}
+        {selected ?? Main}
+      <hr />
     </div>
   );
 }
