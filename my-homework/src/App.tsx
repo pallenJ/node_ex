b543e81275c8dc19b30ef870a7ba871302c4f0f6
JoonMo <pallennellap17@gmail.com>
2021-06-28 18:38:13 +0900

    onBlur로 수정예정

--- my-homework/src/App.tsx
+++ my-homework/src/App.tsx
@@ -59,9 +59,8 @@ function App() {
         moreShow(true,e.target.value);
         console.log(e.target.value)
       }}
-      onFocusCapture = {()=>{console.log('adg')}}
-      onEnded = {()=>{console.log('adg')}}
-      onInput = {()=>{console.log('gd')}}
+
+      onBlur = {()=>{console.log('sdf')}}
       />
       <hr/>
       <div>
